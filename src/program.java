1a472c846063daf06fd5f79a86ed7d2102d431ce
Lewis <22139631@bucks.ac.uk>
2022-10-22 17:16:14 +0100

    it does something

---
+++ src/program.java
@@ -0,0 +1,10 @@
+public class program 
+{
+    public static void main(String[] args) 
+    {
+        String name;
+        System.out.println("Hello, what's your name? ");
+        name = InputReader.getString();
+        System.out.println("Hello" + name + "!");
+    }
+}
